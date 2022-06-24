5dd9b4611e0043cbf439c4199db494eb871edfe5
Nitesh Kumar <nitesh.k3096@gmail.com>
2022-06-24 20:09:58 +0530

    mongoose file updated

--- src/db/mongoose.js
+++ src/db/mongoose.js
@@ -4,31 +4,3 @@ console.log(process.env.MONGO_URL);
 mongoose.connect(
   `${process.env.MONGO_URL}/${process.env.MONGO_DB}?retryWrites=true&w=majority`
 );
-
-// const user = new Users({
-//   name: "Nitesh 4",
-//   email: "Nitesh4@gmail.com",
-//   password: "nitesh@96",
-// });
-
-// user
-//   .save()
-//   .then((data) => {
-//     console.log("saved", data);
-//   })
-//   .catch((data) => {
-//     console.log("failed", data);
-//   });
-
-// const task = new Tasks({
-//   description: "      Setup the folder structire React Native      ",
-// });
-
-// task
-//   .save()
-//   .then((data) => {
-//     console.log("saved", data);
-//   })
-//   .catch((data) => {
-//     console.log("failed", data);
-//   });
