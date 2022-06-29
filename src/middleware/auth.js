2d9d7773e0b3765034e98bdd4b5163e519072446
Nitesh Kumar <nitesh.k3096@gmail.com>
2022-06-29 17:26:49 +0530

    auth keys updated

--- src/middleware/auth.js
+++ src/middleware/auth.js
@@ -1,9 +1,10 @@
 const jwt = require("jsonwebtoken");
 const User = require("../db/schemas/users");
+require("dotenv").config({ path: ".env" });
 const auth = async (req, res, next) => {
   try {
     const token = req.header("Authorization").replace("Bearer ", "");
-    const decoded = jwt.verify(token, "mysecretkey");
+    const decoded = jwt.verify(token, `${process.env.JWT_SECRET}`);
     const user = await User.findOne({
       _id: decoded._id,
       "tokens.token": token,
